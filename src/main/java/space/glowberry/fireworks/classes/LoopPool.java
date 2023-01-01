21610160cb4227f482c1b5579332c9541e8b7af1
等陌上花开 <zhzhn2002@126.com>
2023-01-01 08:44:34 +0800

    <feat>[Arch]: Add the LoopPool
    Add the LoopPool. This is a pool that contains the current loops in the memory.
    
    The purpose is to ensure that the server will not read configure at everytime when user add a loop in game.

---
+++ src/main/java/space/glowberry/fireworks/classes/LoopPool.java
@@ -0,0 +1,28 @@
+package space.glowberry.fireworks.classes;
+
+import java.util.List;
+
+public class LoopPool extends Pool<Loop>{
+
+    private List<Loop> loops;
+    private static final LoopPool instance = new LoopPool();
+
+    @Override
+    public void add(Loop loop) {
+        this.loops.add(loop);
+    }
+
+    @Override
+    public void remove(Loop loop) {
+        this.loops.remove(loop);
+    }
+
+    @Override
+    public List<Loop> getAll() {
+        return this.loops;
+    }
+
+    public static LoopPool getInstance() {
+        return instance;
+    }
+}
